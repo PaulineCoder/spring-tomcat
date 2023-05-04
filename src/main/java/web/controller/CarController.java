dc1a3b5f1f48f07ba2829bb8950a2a7fe9aa5b16
Polina Petrushina <upcode.121@gmail.com>
2023-05-04 12:55:53 +0300

    Changes due to the addition of the dao class

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,28 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
+import web.dao.CarDaoImp;
 import web.service.CarService;
 import web.service.CarServiceImpl;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class CarController {
-    CarService carService = new CarServiceImpl();
+
+    private final CarService carService = new CarServiceImpl(new CarDaoImp());
 
     @GetMapping(value = "/cars")
-    public String printCars(@RequestParam(defaultValue = "-1", value = "count", required = false) Integer count, ModelMap model) {
-        List<Car> cars = new ArrayList<>();
+    public String printCars(@RequestParam(defaultValue = "-1", value = "count", required = false) int count, ModelMap model) {
 
-        cars.add(new Car("BMW", "M2", 87));
-        cars.add(new Car("Mazda", "CX-30", 1));
-        cars.add(new Car("Subaru", "WRX", 4));
-        cars.add(new Car("Acura", "EL", 3));
-        cars.add(new Car("Lexus", "LS", 6));
-
-        model.addAttribute("cars", carService.getCars(cars, count));
+        model.addAttribute("cars", carService.getCars(count));
 
         return "cars";
     }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,18 +1,25 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import web.dao.CarDao;
 import web.model.Car;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Service
 public class CarServiceImpl implements CarService {
+
+    private final CarDao carDao;
+
+    @Autowired
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
     @Override
-    public List<Car> getCars(List<Car> list, int count) {
-        if (count >= 5 || count == -1) {
-            return list;
-        }
-        return list.stream().limit(count).collect(Collectors.toList());
+    public List<Car> getCars(int count) {
+        return carDao.getCars(count);
     }
 }
